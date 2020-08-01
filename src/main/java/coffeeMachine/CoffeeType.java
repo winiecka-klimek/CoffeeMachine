f1159d6d977246762ad18c4e8ec9016a93bfa363
Winiecka-Klimek <marta.winiecka@gmail.com>
2020-08-01 22:48:47 +0200

    first commit

---
+++ src/main/java/coffeeMachine/CoffeeType.java
@@ -0,0 +1,21 @@
+package coffeeMachine;
+
+public enum CoffeeType {
+    //    ESPRESSO(1, 16, 250, 0, 4),
+//    LATTE(2, 20, 350, 75, 7),
+//    CAPPUCCINO(3, 12, 200, 100, 6);
+//
+//    int button;
+//    int coffeeBeans;
+//    int water;
+//    int milk;
+//    int cost;
+//
+//    CoffeeType(int button, int coffeeBeans, int water, int milk, int cost) {
+//        this.button = button;
+//        this.coffeeBeans = coffeeBeans;
+//        this.water = water;
+//        this.milk = milk;
+//        this.cost = cost;
+//    }
+}
---
+++ src/main/java/coffeeMachine/State.java
@@ -0,0 +1,18 @@
+package coffeeMachine;
+
+public enum State {
+
+//
+//    BUY("buy"),
+//    FILL_RESOURCES("fill"),
+//    TAKE_MONEY("take"),
+//    REMAINING("remaining"),
+//    EXIT("exit");
+//
+//    String action;
+//
+//    State(String action) {
+//        this.action = action;
+//    }
+
+}
